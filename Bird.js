94b19c17f00b0cbd9c1420049ee82edd661030f5
akshaya4 <70489515+akshaya4@users.noreply.github.com>
2020-09-29 09:27:46 +0530

    Update Bird.js

--- Bird.js
+++ Bird.js
@@ -6,8 +6,8 @@ class Bird {
         'restitution':0.5
       };
       this.body = Bodies.rectangle(x, y, 50, 50, options);
-      this.width = width;
-      this.height = height;
+      this.width = 50;
+      this.height = 50;
       this.image = loadImage("sprites/bird.png");
       World.add(world, this.body);
       
